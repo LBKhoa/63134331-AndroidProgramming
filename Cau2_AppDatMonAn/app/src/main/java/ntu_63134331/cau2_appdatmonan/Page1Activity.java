81d5929582fb6591ba800235a325574e5da3482b
LBKhoa <152173511+LBKhoa@users.noreply.github.com>
2024-03-25 14:27:58 +0700

    Thêm một vài món ăn

--- Cau2_AppDatMonAn/app/src/main/java/ntu_63134331/cau2_appdatmonan/Page1Activity.java
+++ Cau2_AppDatMonAn/app/src/main/java/ntu_63134331/cau2_appdatmonan/Page1Activity.java
@@ -34,6 +34,15 @@ public class Page1Activity extends AppCompatActivity {
         dsMonAn.add(new MonAn("Cơm chiên tôm",24000,"Cơm chiên có tôm và râu củ",R.drawable.cct));
         dsMonAn.add(new MonAn("Cơm trộn",22000,"Cơm có thịt và trứng",R.drawable.ct));
         dsMonAn.add(new MonAn("Cơm chiên",20000,"Cơm được chiên giòn",R.drawable.cc));
+        dsMonAn.add(new MonAn("Phở bò", 30000, "Món phở truyền thống với thịt bò và nước dùng đậm đà", R.drawable.pho_bo));
+        dsMonAn.add(new MonAn("Gà nướng", 35000, "Gà tươi được nướng thơm phức, với gia vị đặc biệt", R.drawable.ga_nuong));
+        dsMonAn.add(new MonAn("Bánh mì xúc xích", 25000, "Bánh mì mềm được kết hợp với xúc xích thơm ngon", R.drawable.banh_mi_xuc_xich));
+        dsMonAn.add(new MonAn("Bún chả", 28000, "Bún tươi phối hợp với chả nướng và nước mắm chua ngọt", R.drawable.bun_cha));
+        dsMonAn.add(new MonAn("Bún bò Huế", 35000, "Một món bún truyền thống từ Huế, có thịt bò và nước dùng đậm đà", R.drawable.bun_bo_hue));
+        dsMonAn.add(new MonAn("Hủ tiếu Nam Vang", 30000, "Một món hủ tiếu phong phú từ Nam Vang, với hải sản và thịt lợn", R.drawable.hu_tieu_nam_vang));
+        dsMonAn.add(new MonAn("Bánh xèo", 25000, "Bánh xèo giòn rụm với nhân thịt, tôm và rau sống", R.drawable.banh_xeo));
+        dsMonAn.add(new MonAn("Cơm gà Hải Nam", 28000, "Cơm gà nướng vàng ươm từ Hải Nam, kèm theo nước sốt đặc biệt", R.drawable.com_ga_hai_nam));
+
 
         MonAnAdapter adapter = new MonAnAdapter(this,dsMonAn);
         lvDSMonAn.setAdapter(adapter);
@@ -47,13 +56,13 @@ public class Page1Activity extends AppCompatActivity {
                 Toast.makeText(Page1Activity.this,monAnChon.getTenMonAn(),Toast.LENGTH_LONG).show();
             }
         });
-        btnBack = findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //thoát childActivity
-                finish();
-            }
-        });
+//        btnBack = findViewById(R.id.btnBack);
+//        btnBack.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //thoát childActivity
+//                finish();
+//            }
+//        });
     }
 }
