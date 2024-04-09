f8be612e0a22bff2b480d22c7fca970a5b8b964b
LBKhoa <152173511+LBKhoa@users.noreply.github.com>
2024-04-09 12:28:40 +0700

    Lab RecyclerView

---
+++ UsingRecyclerView/app/src/main/java/khoalb/ntu/usingrecyclerview/MainActivity.java
@@ -0,0 +1,49 @@
+package khoalb.ntu.usingrecyclerview;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewData;
+    RecyclerView recyclerViewLandScape;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //3
+        recyclerViewData = getDataForRecyclerView();
+        //4
+        recyclerViewLandScape = findViewById(R.id.recyclerLand);
+        //5
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandScape.setLayoutManager(layoutLinear);
+        //6
+        landScapeAdapter = new LandScapeAdapter(this,recyclerViewData);
+        //7
+        recyclerViewLandScape.setAdapter(landScapeAdapter);
+    }
+    ArrayList<LandScape> getDataForRecyclerView() {
+        ArrayList<LandScape> dsDulieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("kim_tu_thap","Kim Tự Tháp Ai Cập");
+        dsDulieu.add(landScape1);
+        dsDulieu.add(new LandScape("va_ly_truong_thanh","Vạn Lý Trường Thành"));
+        dsDulieu.add(new LandScape("vinh_ha_long","Vịnh Hạ Long"));
+        return dsDulieu;
+    }
+}
