18290eba32ac2616634669105266958dd95b438d
LBKhoa <152173511+LBKhoa@users.noreply.github.com>
2024-04-18 12:19:56 +0700

    Fragment

--- CustomNavigationDrawer/app/src/main/java/khoalb/ntu/customnavigationdrawer/MainActivity.java
+++ CustomNavigationDrawer/app/src/main/java/khoalb/ntu/customnavigationdrawer/MainActivity.java
@@ -1,17 +1,34 @@
 package khoalb.ntu.customnavigationdrawer;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import khoalb.ntu.customnavigationdrawer.fragment.FavoriteFragment;
+import khoalb.ntu.customnavigationdrawer.fragment.HistoryFragment;
+import khoalb.ntu.customnavigationdrawer.fragment.HomeFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int FRAGMENT_HOME = 0;
+    private static final int FRAGMENT_FAVORITE = 1;
+    private static final int FRAGMENT_HISTORY = 2;
+
+    private int mCurrentFragment = FRAGMENT_HOME;
 
     private DrawerLayout mDrawerLayout;
 
@@ -35,5 +52,51 @@ public class MainActivity extends AppCompatActivity {
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        replaceFragment(new HomeFragment());
+
+        navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_home){
+            if (mCurrentFragment != FRAGMENT_HOME){
+                replaceFragment(new HomeFragment());
+                mCurrentFragment = FRAGMENT_HOME;
+            }
+        }else if (id == R.id.nav_favorite){
+            if (mCurrentFragment != FRAGMENT_FAVORITE){
+                replaceFragment(new FavoriteFragment());
+                mCurrentFragment = FRAGMENT_FAVORITE;
+            }
+        }else if (id == R.id.nav_history){
+            if (mCurrentFragment != FRAGMENT_HISTORY){
+                replaceFragment(new HistoryFragment());
+                mCurrentFragment = FRAGMENT_HISTORY;
+            }
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame, fragment);
+        transaction.commit();
     }
 }
