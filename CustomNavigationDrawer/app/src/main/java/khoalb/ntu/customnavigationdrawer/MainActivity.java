be7cc7e711f58331186f9db60573a6827866b01d
LBKhoa <152173511+LBKhoa@users.noreply.github.com>
2024-04-18 10:49:04 +0700

    Logic add toolbar  và sự kiện open Navigation Drawer

--- CustomNavigationDrawer/app/src/main/java/khoalb/ntu/customnavigationdrawer/MainActivity.java
+++ CustomNavigationDrawer/app/src/main/java/khoalb/ntu/customnavigationdrawer/MainActivity.java
@@ -3,12 +3,18 @@ package khoalb.ntu.customnavigationdrawer;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
