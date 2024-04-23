895ae1038e33eafe306fbf8bb3cfe5b049e34177
LBKhoa <152173511+LBKhoa@users.noreply.github.com>
2024-04-23 21:05:53 +0700

    Intent VD1

---
+++ Intent_1/app/src/main/java/khoalb/ntu/intent_1/ChildActivity.java
@@ -0,0 +1,37 @@
+package khoalb.ntu.intent_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChildActivity extends AppCompatActivity {
+    Button btnback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_child);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnback = findViewById(R.id.btnback);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent2 = new Intent(ChildActivity.this,MainActivity.class);
+                startActivity(myIntent2);
+            }
+        });
+    }
+}
