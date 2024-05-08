d5cbc06929aed853dad454d1007e6e303199a2f9
LBKhoa <152173511+LBKhoa@users.noreply.github.com>
2024-05-08 14:07:17 +0700

    QLSinhVien with SQLite ( insert, delete, update, query

---
+++ QLSinhVien_SQLite1/app/src/main/java/khoalb/ntu/qlsinhvien_sqlite1/MainActivity.java
@@ -0,0 +1,145 @@
+package khoalb.ntu.qlsinhvien_sqlite1;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Currency;
+
+public class MainActivity extends AppCompatActivity {
+    //khai báo các biến giao diện
+    EditText edtmalop, edttenlop, edtsiso;
+    Button btninsert, btndelete, btnupdate,btnquery;
+    //khai báo listview
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
+    SQLiteDatabase mydatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //ánh xạ các biên giao diện
+        edtmalop = findViewById(R.id.edtmalop);
+        edttenlop = findViewById(R.id.edttenlop);
+        edtsiso =findViewById(R.id.edtsiso);
+        btninsert = findViewById(R.id.btninsert);
+        btndelete = findViewById(R.id.btndelete);
+        btnupdate = findViewById(R.id.btnupdate);
+        btnquery = findViewById(R.id.btnquery);
+        //tạo listview
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,mylist);
+        lv.setAdapter(myadapter);
+        //tạo và mở cơ sở dũ liệu SQLite
+        mydatabase = openOrCreateDatabase("qlsinhvien.db",MODE_PRIVATE,null);
+        //Tạo database chứa dữ liệu
+        try{
+            String sql = "CREATE TABLE tbllop(malop TEXT primary key, tenlop TEXT, siso INTEGER)";
+            mydatabase.execSQL(sql);
+        }
+        catch (Exception e)
+        {
+            Log.e("Error","Table đã tồn tại");
+        }
+
+        //THIẾT LẬP CÁC NÚT BẤM
+        btninsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String malop = edtmalop.getText().toString();
+                String tenlop = edttenlop.getText().toString();
+                int siso = Integer.parseInt(edtsiso.getText().toString());
+                ContentValues myvalue = new ContentValues();
+                myvalue.put("malop",malop);
+                myvalue.put("tenlop",tenlop);
+                myvalue.put("siso",siso);
+                String msg = "";
+                if (mydatabase.insert("tbllop",null,myvalue) == -1){
+                    msg = "Fail to Insert Record!";
+                }
+                else {
+                    msg = "Insert record Sucessfully";
+                }
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btndelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String malop = edtmalop.getText().toString();
+                int n = mydatabase.delete("tbllop","malop = ?",new String[]{malop});
+                String msg = "";
+                if (n == 0){
+                    msg = "No record to Delete";
+                }
+                else {
+                    msg = n+" record is Delete";
+                }
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnupdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int siso = Integer.parseInt(edtsiso.getText().toString());
+                String malop = edtmalop.getText().toString();
+                ContentValues myvalue = new ContentValues();
+                myvalue.put("siso",siso);
+                int n = mydatabase.update("tbllop",myvalue,"malop = ?",new String[]{malop});
+                String msg = "";
+                if (n == 0){
+                    msg = "No record to Update";
+                }
+                else {
+                    msg = n+" record is Update";
+                }
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnquery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mylist.clear();
+                Cursor c = mydatabase.query("tblop",null,null,null,null,null,null);
+                c.moveToNext();
+                String data = "";
+                while (c.isAfterLast() == false)
+                {
+                    data = c.getString(0)+" - "+c.getString(1)+" - "+c.getString(2);
+                    c.moveToNext();
+                    mylist.add(data);
+                }
+                c.close();
+                myadapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
